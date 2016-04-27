0715379cfefa603fda70ed450e255ed3dfe743db
Chris Stevenson <chris@applauze.com>
2016-04-27 16:03:54 -0700

    Allow analysing a file or a spec

---
+++ lib/overcover/reporter.rb
@@ -0,0 +1,92 @@
+require 'psych'
+
+module Overcover
+
+  class Reporter
+
+    class << self
+
+      def analyse
+        specs = {}
+        Psych.load_stream(File.read('overcover.log')) do |hash|
+          spec_file = hash[:spec]
+          covers = specs[spec_file] || []
+          covers << hash[:files]
+          specs[spec_file] = covers.flatten.uniq
+        end
+        open('overcover_summary.yaml', 'w') do |f|
+          f.puts Psych.dump(specs)
+        end
+        specs
+      end
+
+      def summarize
+        specs = analyse
+
+        spec_report = specs.map { |file, covers|  { file: file, count: covers.count } }
+        spec_report.sort! { |a, b| a[:count] <=> b[:count] }
+        spec_report.reverse!
+
+        reverse = {}
+        specs.each_pair do |spec, covers|
+          covers.each do |cover|
+            reverse[cover] ||= []
+            reverse[cover]<< spec
+          end
+        end
+        file_report = reverse.map { |cover, specs|  { cover: cover, count: specs.count } }
+        file_report.sort! { |a, b| a[:count] <=> b[:count] }
+        file_report.reverse!
+
+        puts
+
+        puts "Overcover report"
+        puts "    #{spec_report.count} specs analysed"
+        puts "    #{file_report.count} files analysed"
+
+        puts "Specs covering the most files:"
+        spec_report.take(10).each do |f|
+          puts "    #{f[:file]} #{f[:count]} "
+        end
+
+        puts "Files covered by the most specs:"
+        file_report.take(10).each do |f|
+          puts "    #{f[:cover]} #{f[:count]} "
+        end
+
+      end
+
+    end
+
+    def self.analyse_spec(f)
+      specs = analyse
+
+      puts "Overcover report"
+      puts "Spec file: #{f}"
+      specs[f].each do |cover|
+        puts "    #{cover}"
+      end
+
+    end
+
+    def self.analyse_file(f)
+      specs = analyse
+      reverse = {}
+      specs.each_pair do |spec, covers|
+        covers.each do |cover|
+          reverse[cover] ||= []
+          reverse[cover]<< spec
+        end
+      end
+
+      puts "Overcover report"
+      puts "Production file: #{f}"
+      reverse[f].each do |cover|
+        puts "    #{cover}"
+      end
+
+    end
+
+  end
+
+end
